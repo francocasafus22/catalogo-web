e495727afd0500cebf186342bd18fcb3af4aab26
francocasafus22 <francocasafus55@gmail.com>
2026-05-10 22:18:25 -0300

    add hero

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,57 @@
 import Navbar from "@/components/Navbar";
+import { Button } from "@/components/ui/button";
+import { MessageCircle } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center">
+    <div className="flex flex-col flex-1 bg-background">
       <Navbar />
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
-        
-        <h1 className="text-5xl font-bold text-center sm:text-left text-primary">Catalogo</h1>
 
-      </main> 
+      <section className="relative w-full min-h-[80vh] overflow-hidden bg-primary  text-primary-foreground">    
+
+        <div className="relative mx-auto flex min-h-[80vh] justify-center text-center items-center gap-16 px-6 py-12">
+                    
+          <div className="flex flex-col items-center">
+            
+            <span className="mb-4 w-fit rounded-full border border-[#df0909]/20 bg-white/70 px-4 py-1 text-sm font-medium text-[#df0909] backdrop-blur-sm">
+              NUEVA COLECCIÓN
+            </span>
+
+            <h1 className="mb-6 text-5xl font-black tracking-tight text-[#df0909] md:text-7xl">
+              Mariana Duran
+            </h1>
+
+            <p className="mb-8 max-w-xl text-lg text-foreground/80 md:text-xl">
+              Descubrí nuestros nuevos ingresos y comprá directo por WhatsApp.
+            </p>
+
+            <div className="flex justify-center items-center gap-4">
+              
+              <Button
+                size="lg"
+                className="bg-[#df0909] text-white hover:bg-[#c40808]"
+              >
+                Ver catálogo
+              </Button>
+
+              <Button
+                variant="secondary"
+                size="lg"
+                className="border border-border bg-white/80 backdrop-blur-sm"
+              >
+                <MessageCircle />
+                WhatsApp
+              </Button>
+            </div>
+          </div>
+          
+        </div>
+      </section>
+
+      <main className="flex-1 p-4">
+        {/* Productos */}
+      </main>
     </div>
   );
 }
--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,9 +1,12 @@
 import { ModeToggle } from "./mode-toggle";
+import { Input } from "./ui/input";
 
 export default function Navbar() {
     return (
         <nav className="w-full flex items-center justify-between p-4">
-            <p className="font-bold text-primary text-xl md:text-3xl">Mariana</p>
+            
+            <p className="font-bold text-brand-red text-xl md:text-3xl">Mariana</p>
+            <Input placeholder="Search" className="max-w-2xl border-0 bg-primary focus:ring-primary" />
             <ModeToggle />
         </nav>
     )
